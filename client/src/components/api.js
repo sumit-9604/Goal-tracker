25ccc43577214e86fece69787c84800c9cf88c16
SUMIT <sumit.4814802723@cse.mait.ac.in>
2026-05-17 23:35:50 +0530

    fix: use env var for API baseURL instead of hardcoded URL

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://goal-tracker-o8qg.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   headers: {
     "Content-Type": "application/json",
   },
